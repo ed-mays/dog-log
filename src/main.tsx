dc6b21f6dd0445c09eaff071d995092089ee7381
Ed Mays <copperstarsystems@gmail.com>
2025-09-06 17:28:18 -0500

    remove stranded .js files

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import './i18n.tsx';
-import { FeatureFlagsProvider } from './featureFlags/FeatureFlagsContext.tsx';
+import { FeatureFlagsProvider } from './featureFlags/FeatureFlagsProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
