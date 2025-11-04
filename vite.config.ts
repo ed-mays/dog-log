16ee3fe8fd3f6ca8886c3544f26bcc2e4c8361ae
Ed Mays <copperstarsystems@gmail.com>
2025-11-03 20:52:11 -0500

    configure rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig(() => ({
+export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-}));
+  build: {
+    rollupOptions: {
+      plugins: [visualizer({ filename: 'dist/stats.html', open: true })],
+    },
+  },
+});
